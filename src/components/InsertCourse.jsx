ed1538c6713763ffb79bf63a3a6284642bef60ce
sivagangaaa123 <sivagangaaa123@gmail.com>
2026-07-11 14:30:29 +0530

    Insert Course

---
+++ src/components/InsertCourse.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const InsertCourse = () => {
+  return (
+    <div>
+        <h1><center>Insert Course</center></h1>
+
+
+    </div>
+  )
+}
+
+export default InsertCourse
